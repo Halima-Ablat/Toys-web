8bbbdca62d97e661767045066f819e7fb65bbff7
Halima <152661929+Halima-Ablat@users.noreply.github.com>
2024-11-15 21:50:14 +1100

    Add files via upload

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,18 +1,17 @@
+import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
 function Home() {
   return (
     <>
-      <nav className="navbar navbar-light">
-        <div className="container-fluid">
-          <a className="navbar-brand text-white fs-2" href="#">
-            More toys
-          </a>
-        </div>
-      </nav>
-      <div className=" container-fluid p-0">
-        <div className="back-image text-white text-center">
-          <div className="front-font">
-            <p>Hand made toys</p>
-            <h1>Welcome to Adeena's world</h1>
+      <div className="background-image">
+        <Navbar />
+        <div className="container-fluid p-0">
+          <div className="back-image text-white text-center">
+            <div className="front-font">
+              <p>Hand made toys</p>
+              <h1>Welcome to Adeena's world</h1>
+            </div>
           </div>
         </div>
       </div>
---
+++ pages/Toys.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+import Card from "../components/Card";
+import Slides from "../components/Slides";
+function Toys() {
+  return (
+    <>
+      <div className="pink-page">
+        <Navbar />
+        <Slides />
+        <Card />
+      </div>
+    </>
+  );
+}
+export default Toys;
